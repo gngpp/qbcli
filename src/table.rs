25f193a532a1f3c450234bb0184971c6fdeb39cf
remake <verticle@foxmail.com>
2022-06-12 11:42:32 +0800

    feat: update print data table api

--- src/table.rs
+++ src/table.rs
@@ -4,7 +4,6 @@ use prettytable::{color, Attr, Cell, Row};
 pub(crate) fn print_query_qq_table(res: req::DataResult) {
     let status = res.code.to_string();
     let msg = res.msg.unwrap_or_default();
-
     let data = res.data.unwrap_or_default();
     let qq = data.qq.unwrap_or_default();
     let mobile = data.mobile.unwrap_or_default();
@@ -24,8 +23,8 @@ pub(crate) fn print_query_qq_table(res: req::DataResult) {
         Cell::new(lol_data.area.unwrap_or_default().as_str()),
     ]));
 
-    let mut data_table = prettytable::Table::new();
-    data_table.add_row(Row::new(vec![
+    let mut table = prettytable::Table::new();
+    table.add_row(Row::new(vec![
         Cell::new("status")
             .with_style(Attr::Bold)
             .with_style(Attr::ForegroundColor(color::GREEN)),
@@ -49,7 +48,7 @@ pub(crate) fn print_query_qq_table(res: req::DataResult) {
             .with_style(Attr::ForegroundColor(color::GREEN)),
     ]));
 
-    data_table.add_row(Row::new(vec![
+    table.add_row(Row::new(vec![
         Cell::new(status.as_str()),
         Cell::new(msg.as_str()),
         Cell::new(qq.as_str()),
@@ -59,13 +58,13 @@ pub(crate) fn print_query_qq_table(res: req::DataResult) {
         Cell::new(lol_table.to_string().as_str()),
     ]));
 
-    data_table.printstd();
+    table.printstd();
 }
 
+//noinspection DuplicatedCode
 pub(crate) fn print_reverse_query_qq_table(res: req::DataResult) {
     let status = res.code.to_string();
     let msg = res.msg.unwrap_or_default();
-
     let data = res.data.unwrap_or_default();
     let qq = data.qq.unwrap_or_default();
     let mobile = data.mobile.unwrap_or_default();
@@ -83,7 +82,7 @@ pub(crate) fn print_reverse_query_qq_table(res: req::DataResult) {
             .with_style(Attr::ForegroundColor(color::GREEN)),
         Cell::new("mobile")
             .with_style(Attr::Bold)
-            .with_style(Attr::ForegroundColor(color::GREEN))
+            .with_style(Attr::ForegroundColor(color::GREEN)),
     ]));
 
     table.add_row(Row::new(vec![
@@ -94,4 +93,140 @@ pub(crate) fn print_reverse_query_qq_table(res: req::DataResult) {
     ]));
 
     table.printstd();
+}
+
+//noinspection DuplicatedCode
+pub(crate) fn print_query_weibo_table(res: req::DataResult) {
+    let status = res.code.to_string();
+    let msg = res.msg.unwrap_or_default();
+    let place = res.place.unwrap_or_default();
+    let data = res.data.unwrap_or_default();
+    let uid = data.uid.unwrap_or_default();
+    let mobile = data.mobile.unwrap_or_default();
+
+    let mut table = prettytable::Table::new();
+    table.add_row(Row::new(vec![
+        Cell::new("status")
+            .with_style(Attr::Bold)
+            .with_style(Attr::ForegroundColor(color::GREEN)),
+        Cell::new("message")
+            .with_style(Attr::Bold)
+            .with_style(Attr::ForegroundColor(color::GREEN)),
+        Cell::new("place")
+            .with_style(Attr::Bold)
+            .with_style(Attr::ForegroundColor(color::GREEN)),
+        Cell::new("uid")
+            .with_style(Attr::Bold)
+            .with_style(Attr::ForegroundColor(color::GREEN)),
+        Cell::new("mobile")
+            .with_style(Attr::Bold)
+            .with_style(Attr::ForegroundColor(color::GREEN)),
+    ]));
+
+    table.add_row(Row::new(vec![
+        Cell::new(status.as_str()),
+        Cell::new(msg.as_str()),
+        Cell::new(place.as_str()),
+        Cell::new(uid.as_str()),
+        Cell::new(mobile.as_str()),
+    ]));
+
+    table.printstd();
+}
+
+//noinspection DuplicatedCode
+pub(crate) fn print_reverse_query_weibo_table(res: req::DataResult) {
+    let status = res.code.to_string();
+    let msg = res.msg.unwrap_or_default();
+    let data = res.data.unwrap_or_default();
+    let uid = data.uid.unwrap_or_default();
+    let mobile = data.mobile.unwrap_or_default();
+
+    let mut table = prettytable::Table::new();
+    table.add_row(Row::new(vec![
+        Cell::new("status")
+            .with_style(Attr::Bold)
+            .with_style(Attr::ForegroundColor(color::GREEN)),
+        Cell::new("message")
+            .with_style(Attr::Bold)
+            .with_style(Attr::ForegroundColor(color::GREEN)),
+        Cell::new("uid")
+            .with_style(Attr::Bold)
+            .with_style(Attr::ForegroundColor(color::GREEN)),
+        Cell::new("mobile")
+            .with_style(Attr::Bold)
+            .with_style(Attr::ForegroundColor(color::GREEN)),
+    ]));
+
+    table.add_row(Row::new(vec![
+        Cell::new(status.as_str()),
+        Cell::new(msg.as_str()),
+        Cell::new(uid.as_str()),
+        Cell::new(mobile.as_str()),
+    ]));
+
+    table.printstd();
+}
+
+pub(crate) fn print_query_lol_table(res: req::DataResult) {
+    let status = res.code.to_string();
+    let msg = res.msg.unwrap_or_default();
+    let data = res.data.unwrap_or_default();
+    let qq = data.qq.unwrap_or_default();
+
+    let mut table = prettytable::Table::new();
+    table.add_row(Row::new(vec![
+        Cell::new("status")
+            .with_style(Attr::Bold)
+            .with_style(Attr::ForegroundColor(color::GREEN)),
+        Cell::new("message")
+            .with_style(Attr::Bold)
+            .with_style(Attr::ForegroundColor(color::GREEN)),
+        Cell::new("qq")
+            .with_style(Attr::Bold)
+            .with_style(Attr::ForegroundColor(color::GREEN))
+    ]));
+
+    table.add_row(Row::new(vec![
+        Cell::new(status.as_str()),
+        Cell::new(msg.as_str()),
+        Cell::new(qq.as_str())
+    ]));
+}
+
+//noinspection DuplicatedCode
+pub(crate) fn print_reverse_query_lol_table(res: req::DataResult) {
+    let status = res.code.to_string();
+    let msg = res.msg.unwrap_or_default();
+    let data = res.data.unwrap_or_default();
+    let name = data.name.unwrap_or_default();
+    let qq = data.qq.unwrap_or_default();
+    let dq = data.dq.unwrap_or_default();
+
+    let mut table = prettytable::Table::new();
+    table.add_row(Row::new(vec![
+        Cell::new("status")
+            .with_style(Attr::Bold)
+            .with_style(Attr::ForegroundColor(color::GREEN)),
+        Cell::new("message")
+            .with_style(Attr::Bold)
+            .with_style(Attr::ForegroundColor(color::GREEN)),
+        Cell::new("name")
+            .with_style(Attr::Bold)
+            .with_style(Attr::ForegroundColor(color::GREEN)),
+        Cell::new("qq")
+            .with_style(Attr::Bold)
+            .with_style(Attr::ForegroundColor(color::GREEN)),
+        Cell::new("dq")
+            .with_style(Attr::Bold)
+            .with_style(Attr::ForegroundColor(color::GREEN)),
+    ]));
+
+    table.add_row(Row::new(vec![
+        Cell::new(status.as_str()),
+        Cell::new(msg.as_str()),
+        Cell::new(name.as_str()),
+        Cell::new(qq.as_str()),
+        Cell::new(dq.as_str())
+    ]));
 }
